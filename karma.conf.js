f140742c8fba0b7aef5bc0801b186ef21eec2cdc
david-bulte <david.bulte@gmail.com>
2021-11-10 22:28:27 +0100

    rename project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/blobbie'),
+      dir: require('path').join(__dirname, './coverage/generative-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
